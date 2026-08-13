2d42f4532cbca6ab2298d2968625d19f4c4ee024
sq09491-netizen <sq09491@gmail.com>
2026-08-13 17:33:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -14,6 +14,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+ssl: { rejectUnauthorized: false }
 });
 
 // Fail fast at startup if the DB is unreachable.
