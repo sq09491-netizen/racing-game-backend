7c7558663480b8b03ec6dc444c8afed7c303aa08
sq09491-netizen <sq09491@gmail.com>
2026-08-13 20:57:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,  'js')));
+app.use(express.static(path.join(__dirname,  )));
 
 // Jab koi direct URL khole tab index.html serve ho:
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'js', 'index.html'));
+    res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 app.use('/api/auth', authRoutes);
